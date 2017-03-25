110206041615b1760fba9686908d754c3a74b329
Samantha Soto <samanthasoto@Samanthas-MacBook-Air-2.local>
2017-03-25 13:16:43 -0400

    Fixed responsive navbar.Created remaining sections of website. Added content to about and portfolio section including listed thumbnail images.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function myFunction() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += "responsive";
-	} else {
-		x.className = "topnav";
-	}
+function navBarFunction() {
+	document.getElementsByClassName("navbar")
+[0].classList.toggle("responsive");
 }
